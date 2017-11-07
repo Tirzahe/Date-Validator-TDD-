ee1a608b28ff9cf3d7b642c5b8c90a79b245c126
tirzahe <tirzah410@gmail.com>
2017-11-07 11:09:47 -0700

    Committing with new functions: hasValid Delimeters, hasValidYear, hasValidDay

--- src/script.js
+++ src/script.js
@@ -1,15 +1,6 @@
 function isValidDate(a, b) {
    
 }
-
-// function alertValid(){
-//     //if true function alert("this is a valid date");
-// }
-
-// function alertInvalid(){
-//     //else if false alert("this is an invalid date")
-// }
-
 function hasValidLength(date){
     return date.length === 8 || date.length === 10;
 }
@@ -19,9 +10,53 @@ function hasValidMonth(date){
     var mmPrefix = ['01', '02','03','04','05','06','07','08','09','10','11','12'];
     return mmPrefix.includes(date.slice(0,2));
 }
+function hasValidDelimiters(date){
+    var delimeters = ['/','-','.'];
+    return delimeters.includes(date[2]) && delimeters.includes(date[5]) && (date[2] === date[5]);
+}
+function hasValidYear(date){
+    var num = ['0','1','2','3','4','5','6','7','8','9'];
+    if(date.length === 8){
+        return (num.includes(date[6]) && num.includes(date[7]));
+    }
+    else if(date.length === 10){
+        return (num.includes(date[6]) && num.includes(date[7]) && num.includes(date[8]) && num.includes(date[9]));
+    }
+    else{
+        return false;
+    }
+}
+function hasValidDay(date){
+    var mo30Days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];
+    var mo31Days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+    var feb28Days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28'];
+    var feb29Days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29'];
+    var year = date.slice(-2);
+    if((date.slice(0, 2) === '01') || (date.slice(0, 2) ==='03') || (date.slice(0, 2) === '05') || (date.slice(0, 2) ==='07')|| (date.slice(0, 2) === '08') || (date.slice(0, 2) === '10') || (date.slice(0, 2) === '12')) {
+        return mo31Days.includes(date.slice(3, 5));
+    }
+    else if((date.slice(0, 2) === '04') || (date.slice(0, 2) === '06') || (date.slice(0, 2) === '09') || (date.slice(0, 2) === '11')){
+        return mo30Days.includes(date.slice(3, 5));
+    }
+    else if((date.slice(0, 2) === '02') && (year % 4 === 0)){
+        return feb29Days.includes(date.slice(3, 5));
+    }
+    else if((date.slice(0, 2) === '02') && (year % 4 != 0)){
+        return feb28Days.includes(date.slice(3, 5));
+    }
+    else{
+        return false;
+    }
+}
+    
+// function alertValid(){
+  //  if(isValidDate === true){
+//     alert("this is a valid date");
+// }
 
-// function hasValidFormat(date){
-//     for (i=0; i< date.length; i++){
-//         //if (date.slice(0,2) === 
-//     }
+// function alertInvalid(){
+//     //else if false alert("this is an invalid date")
+// }
+// else {
+//         return alert("This is an invalid date, Please try again!")
 // }
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -28,38 +28,59 @@ describe('isValidDate', function() {
       expect(hasValidMonth('MAR-12-1919')).toBe(false);//has letters
       expect(hasValidMonth('133-12-2014')).toBe(false); // has 3 digits
       expect(hasValidMonth('13-12-2014')).toBe(false); // has wrong digits
+      expect(hasValidMonth('00-12-2014')).toBe(false); // has wrong digits
     });
-    it()
-    // it('has 2 valid digits in the correct index locations 3 & 4, to create any number from 1-31, and will inly indicate the '), function(){
-    //   expect(hasValidDay('01-01-2001')).toBe(true);
-    //   expect(hasValidDay('02-28-2001')).toBe(true);
+    it('has delimeters "/", "-" or "." in index position 2 and 5 that are the same', function(){
+      expect(hasValidDelimiters('03-12-09')).toBe(true); //has "-" for delimeter
+      expect(hasValidDelimiters('03/12/09')).toBe(true); //has "/" for delimeter
+      expect(hasValidDelimiters('03.12.09')).toBe(true); //has "-" for delimeter
 
-    //   expect(hasValidDay('01-1-2001')).toBe(false);
-    //   expect(hasValidDay('01-1-2001')).toBe(false);
-    //   expect(hasValidDay('01-1-2001')).toBe(false);
-    //   expect(hasValidDay('01-1-2001')).toBe(false);
-    // }
+      expect(hasValidDelimiters('03121909')).toBe(false); //has no delimeter
+      expect(hasValidDelimiters('03*12*09')).toBe(false); //has invalid delimeter
+      expect(hasValidDelimiters('03_12_09')).toBe(false); //has invalid delimeter
+      expect(hasValidDelimiters('03\'12\'09')).toBe(false); //has invalid delimeter
+      expect(hasValidDelimiters('03\\12\\09')).toBe(false); //has invalid delimeter
+
+      expect(hasValidDelimiters('03-12/09')).toBe(false); //has two different delimeters
+
+    });
   });
-  // describe('hasValidFormat', function(){
-  //   it('has two digits for Month, two digits for day and two or four digits for year', function(){
-  //     expect(hasValidFormat('03-12-19')).toBe(true);
-  //     // expect(hasValidFormat('03-12-1919')).toBe(true);
+  describe('hasValidYear', function(){
+    it('has two or four numbers in the index locations 6 & 7 or 6,7,8 & 9', function(){
+      expect(hasValidYear('01-01-01')).toBe(true);
+      expect(hasValidYear('01-01-2001')).toBe(true);
 
-  //     // expect(hasValidFormat('3-12-19')).toBe(false); // 1 digit for month
-  //     // expect(hasValidFormat('030-12-19')).toBe(false); // 3 digits for month
-  //     // expect(hasValidFormat('03-2-19')).toBe(false); // 1 digit for day
-  //     // expect(hasValidFormat('03-112-19')).toBe(false);// 3 digits for day
-  //     // expect(hasValidFormat('03-12-9')).toBe(false);// 1 digit for year
-  //     // expect(hasValidFormat('03-12-191')).toBe(false); // 3 digits for year
-  //     // expect(hasValidFormat('03-12-19191')).toBe(false); //5 digits for year
-  //   });
-  //   it('is only numbers and has "-", "/" or "." as seperators', function(){
-  //     expect(hasValidFormat('03-12-19')).toBe(true);
-  //     expect(hasValidFormat('03-12-1919')).toBe(true);
+      expect(hasValidYear('01-01-0')).toBe(false);
+      expect(hasValidYear('01-01-001')).toBe(false);
+      expect(hasValidYear('01-01-0e')).toBe(false);
+      expect(hasValidYear('01-01-200e')).toBe(false);
+    });
+  });
+  describe('hasValidDays', function(){
+    it('if the month is: 01,03,05,07,08,10,12 it will have a 2 digit number including and between 01-31 for the day in index positions 3 & 4', function(){
+      expect(hasValidDay('01-01-01')).toBe(true);
+      expect(hasValidDay('02-29-12')).toBe(true); // feb 29 days in a leap year
+      expect(hasValidDay('01-31-2001')).toBe(true);
+      expect(hasValidDay('04-30-2012')).toBe(true);
 
-  //     expect(hasValidFormat('MAR-12-19')).toBe(false); // has letters
-  //     expect(hasValidFormat('03121919')).toBe(false); // has no seperator
-  //     expect(hasValidFormat('03_12_19')).toBe(false); // has invalid seperator
-  //   });
-  // });
+      expect(hasValidDay('01-1-01')).toBe(false); // has 1 digit in Day section
+      expect(hasValidDay('01-0e-01')).toBe(false); // has letter in day section
+      expect(hasValidDay('01-014-01')).toBe(false); // has 3 digits for day section (THIS TEST FAILS, WHY??)
+      expect(hasValidDay('01-414-01')).toBe(false); // has 3 digits for day section (THIS TEST PASSES, why this and not the one above??)
+      expect(hasValidDay('02-29-01')).toBe(false); // has 29 days in Feb non leap year
+      expect(hasValidDay('02-30-12')).toBe(false); // has 30 days in Feb Leap year
+      expect(hasValidDay('01-00-01')).toBe(false); // has 0 days
+      expect(hasValidDay('01-32-01')).toBe(false); // has 32 days in Jan
+      expect(hasValidDay('03-32-01')).toBe(false); // has 32 days in Mar
+      expect(hasValidDay('05-32-01')).toBe(false); // has 32 days in May
+      expect(hasValidDay('07-32-01')).toBe(false); // has 32 days in Jul
+      expect(hasValidDay('08-32-01')).toBe(false); // has 32 days in Aug
+      expect(hasValidDay('10-32-01')).toBe(false); // has 32 days in Oct
+      expect(hasValidDay('12-32-01')).toBe(false); // has 32 days in Dec
+      expect(hasValidDay('04-31-01')).toBe(false); // has 31 days in Apr
+      expect(hasValidDay('06-31-01')).toBe(false); // has 31 days in Jun
+      expect(hasValidDay('09-31-01')).toBe(false); // has 31 days in Sep
+      expect(hasValidDay('11-31-01')).toBe(false); // has 31 days in Nov
+    });
+  });
 });
